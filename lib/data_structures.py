b37b51a49b5a2dac293a8da696f1fd12cc5f6450
MUSHABE <mushabe.martin@student.moringaschool.com>
2023-11-27 09:28:31 +0300

    completed

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -13,26 +13,35 @@ spicy_foods = [
         "name": "Mapo Tofu",
         "cuisine": "Sichuan",
         "heat_level": 6,
-    },
+    }
 ]
 
+# Function to get names of spicy foods
 def get_names(spicy_foods):
-    pass
+    return [food["name"] for food in spicy_foods]
 
+# Function to get spiciest foods
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
+# Function to print spicy foods with emojis representing heat level
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        heat_level_emojis = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level_emojis}")
 
+# Function to get spicy food by cuisine
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    return next((food for food in spicy_foods if food["cuisine"] == cuisine), None)
 
+# Function to print spiciest foods with heat level greater than 5
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spiciest_foods)
 
-def get_average_heat_level(spicy_foods):
-    pass
-
-def create_spicy_food(spicy_foods, spicy_food):
-    pass
+# Function to get average heat level
+def average_heat_level(spicy_foods):
+    if not spicy_foods:
+        return 0
+    total_heat_level = sum(food["heat_level"] for food in spicy_foods)
+    return total_heat_level // len(spicy_foods)
